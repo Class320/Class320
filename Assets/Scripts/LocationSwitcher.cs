33e71624a38e6dec22a391dcadc71e56c28a780a
Chamber6821 <vania.osinin@yandex.ru>
2021-11-29 22:50:01 +0300

    feat: add script LocationSwitcher

---
+++ Assets/Scripts/LocationSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Target;
+
+    [SerializeField]
+    private GameObject Location;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Target)
+        {
+            Location.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == Target)
+        {
+            Location.SetActive(false);
+        }
+    }
+}
